0a1c5f06f9dea1d2af45fbd33800c590f6f8ac78
Andreu <andreulinaresfp@ibf.cat>
2025-03-29 14:01:07 +0100

    problema con materiales solucionado

--- Assets/Scripts/MoveClick.cs
+++ Assets/Scripts/MoveClick.cs
@@ -7,27 +7,31 @@ public class MoveClick : MonoBehaviour
 {
 
     public Camera mainCamera;
+
+    public GameObject destination;
     public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        destination = GameObject.FindGameObjectWithTag("Destination");
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        /*if(Input.GetMouseButtonDown(0)){
             Move();
-        }
+        }*/
     }
 
-    void Move(){
+    /*void Move(){
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         if(Physics.Raycast(ray, out hit)){
             agent.SetDestination(hit.point);
         }
-    }
+    }*/
 }
