945444b3a52f4fc34bc640c5a6076bb5b6bad172
Andreu <andreulinaresfp@ibf.cat>
2025-04-10 18:27:26 +0200

    commit prueba

--- Assets/Scripts/MeleeDetection.cs
+++ Assets/Scripts/MeleeDetection.cs
@@ -14,12 +14,15 @@ public class MeleeDetection : MonoBehaviour
         if (other.CompareTag("Melee")){
             if(gameObject.name == "DetectarMelee"){
                 enemigoDetectado = true;
+                Debug.Log("Melee detectado");
             }else if(gameObject.name == "AreaInfluencia"){
                 enemigoEstaEnAreaInfluencia = true;
+                Debug.Log("El melee esta en la area de influencia");
             }
         }else if (other.CompareTag("Player")){
             if(gameObject.name == "DetectarJugador"){
                 jugadorDetectado = true;
+                Debug.Log("Player detectado");
             }
         }
     }
@@ -29,12 +32,15 @@ public class MeleeDetection : MonoBehaviour
         if (other.CompareTag("Melee")){
             if(gameObject.name == "DetectarMelee"){
                 enemigoDetectado = false;
+                Debug.Log("No detecto ningun enemigo melee");
             }else if(gameObject.name == "AreaInfluencia"){
                 enemigoEstaEnAreaInfluencia = false;
+                Debug.Log("El melee ya no esta en el area de influencia");
             }
         }else if (other.CompareTag("Player")){
             if(gameObject.name == "DetectarJugador"){
                 jugadorDetectado = false;
+                Debug.Log("No detecto ningun jugador");
             }
         }
     }
