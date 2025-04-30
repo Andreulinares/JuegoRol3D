119821e0fe6b4b3dd769948e7fa8920cdab3af0c
Andreu <andreulinaresfp@ibf.cat>
2025-04-30 13:17:28 +0200

    textura escalera + menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject panelOpciones;
+    public GameObject panelControles;
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("escena"); 
+    }
+
+    public void AbrirOpciones()
+    {
+        panelOpciones.SetActive(true); 
+    }
+
+    public void CerrarOpciones()
+    {
+        panelOpciones.SetActive(false); 
+    }
+
+    public void AbrirControles()
+    {
+        panelControles.SetActive(true); 
+    }
+
+    public void CerrarControles()
+    {
+        panelControles.SetActive(false); 
+    }
+
+    public void salir(){
+        //salir
+    }
+}
