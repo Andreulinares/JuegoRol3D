2d5387d2e10f4f8303c97a58e0397a3d1dfb2997
Jolen_19 <joelp.xir@gmail.com>
2025-05-18 00:21:01 +0200

    d

--- Assets/Scripts/UI/Diario.cs
+++ Assets/Scripts/UI/Diario.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Diario : MonoBehaviour
 {
     public GameObject panelDiario;
+    public GameObject[] paginas = new GameObject[7];
+    public KeyCode teclaAvanzar     = KeyCode.RightArrow;
+    public KeyCode teclaRetroceder  = KeyCode.LeftArrow;
     private bool diarioAbierto = false;
+    private int paginaActual   = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,17 @@ public class Diario : MonoBehaviour
         {
             ToggleDiario();
         }
+        if (diarioAbierto)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                CambiarPagina(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                CambiarPagina(-1);
+            }
+        }
     }
 
     private void ToggleDiario()
@@ -27,5 +42,36 @@ public class Diario : MonoBehaviour
         panelDiario.SetActive(diarioAbierto);
         
         Time.timeScale = diarioAbierto ? 0 : 1;
+        if (diarioAbierto)
+        {
+            MostrarPagina(paginaActual);
+        }
+        else
+        {
+            DesactivarTodasLasPaginas();
+        }
+    }
+    private void CambiarPagina(int direccion)
+    {
+        paginaActual += direccion;
+
+        // Limitar el rango entre 0 y paginas.Length - 1
+        paginaActual = Mathf.Clamp(paginaActual, 0, paginas.Length - 1);
+
+        MostrarPagina(paginaActual);
+    }
+    private void MostrarPagina(int indice)
+    {
+        for (int i = 0; i < paginas.Length; i++)
+        {
+            paginas[i].SetActive(i == indice);
+        }
+    }
+    private void DesactivarTodasLasPaginas()
+    {
+        foreach (GameObject pagina in paginas)
+        {
+            pagina.SetActive(false);
+        }
     }
 }
